d266e8b4e39323c55ced1619911f128c06fe9d2c
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-25 15:00:02 +0000

    working for post, new task

--- controllers/tasks_controller.py
+++ controllers/tasks_controller.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, redirect, request
 from repositories import task_repository
 import repositories.user_repository as user_repository
+from models.task import Task
 
 
 tasks_blueprint = Blueprint("tasks", __name__)
@@ -16,7 +17,7 @@ def tasks():
 
 
 # NEW
-# GET '/tasks/new' ==> show html form to create a new task
+# GET '/tasks/new' -> show html form to create a new task
 # We added the methods variable to be SPECIFIC that only want GET requests here.
 @tasks_blueprint.route("/tasks/new", methods=["GET"])
 def new_task():
@@ -25,20 +26,39 @@ def new_task():
     return render_template("tasks/new.html", all_users=users)
 
 # CREATE
-# POST '/tasks' ==> handle the POST from the new form
+# POST '/tasks' -> handle the POST from the new form
 # Note: This will be the same route as the on defined before the tasks() function
 @tasks_blueprint.route("/tasks", methods=["POST"])
 def create_task():
-    print("post was called")
+    # grab the form data for the description, user_id, duraton and completed
+    description = request.form['description']
+    user_id = request.form['user_id']
+    duration = request.form['duration']
+    completed = request.form['completed']
+    
+    # select the user using the repository
+    user = user_repository.select(user_id)
+    
+    # create a new task object
+    task = Task(description, user, duration, completed)
+
+    # save that new task object back to the database with the save method
+    task_repository.save(task)
+
+    return redirect('/tasks')
+
 
 # SHOW
-# GET '/tasks/<id>' ==> Show some html for a specific task
+# GET '/tasks/<id>' -> Show some html for a specific task
+
 
 # EDIT
-# GET '/tasks/<id>/edit' ==> Show some html form to edit a specific task
+# GET '/tasks/<id>/edit' -> Show some html form to edit a specific task
+
 
 # UPDATE
-# PUT '/tasks/<id>' ==> handle the PUT from the edit form
+# PUT '/tasks/<id>' -> handle the PUT from the edit form
+
 
 # DELETE
-# DELETE '/tasks/<id>' ==> Handle the delete - to delete a specific task
+# DELETE '/tasks/<id>' -> Handle the delete - to delete a specific task
