c81e4b7956e1174dfe1a2bdfc0365814e6a9459e
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-25 10:35:54 +0000

    registered blueprint to app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
+
+# from the controllers directory (a package), file (a module) tasks_controller import tasks_blueprint (a route)
+from controllers.tasks_controller import tasks_blueprint
+
 app = Flask(__name__)
+
+
+# 'bring' the blueprint route into this module
+app.register_blueprint(tasks_blueprint)
 
 @app.route('/')
 def home():
